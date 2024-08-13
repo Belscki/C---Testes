fbd06328e164aebf140e5e3a20634ab4ad17d2ab
Belscki <136617733+Belscki@users.noreply.github.com>
2024-08-13 12:10:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            Console.WriteLine("Olá, qual seu nome ?");
+            nome = Console.ReadLine();
+            
+            Console.WriteLine("Muito Prazer! " + nome);
+            
+        }
+    }
+}
