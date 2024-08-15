b526def89bdfdef99cca611875a94fe1e0d9d9b8
Belscki <136617733+Belscki@users.noreply.github.com>
2024-08-15 12:36:40 -0300

    Add files via upload

---
+++ IfElse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args){
+            // if (15 > 18)
+            // {
+            //     Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            // }
+            // // else if
+            // // {
+            //         // Mesma coisa
+            // // }
+            // else
+            // {
+            //     Console.WriteLine("bbbbbbbbbbbbbbbbbbbbbbbbb");
+            // }
+
+            ////////////////////////////////////////////////////////
+            
+            int time = 16;
+            string result = (time < 18) ? "Bom dia!" : "Boa noite!";  // Dessa forma o '?' funciona como if e o ':' como else
+            Console.WriteLine(result);
+        }
+    }
+}
