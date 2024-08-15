a68c5f38884fd97caafce5012b7fdeb7ef65b9d0
Belscki <136617733+Belscki@users.noreply.github.com>
2024-08-15 12:36:04 -0300

    Add files via upload

---
+++ ForLoop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int linhas = 10;
+
+            for(int i = 1; i <= linhas; i++)
+                {
+                    for(int j = 1; j<= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                Console.WriteLine();
+                }
+            
+            // foreach para percorrer arrays
+            // string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+            // foreach (string i in cars) 
+            // {
+            //   Console.WriteLine(i);
+            // } 
+        }
+        
+    }
+}
