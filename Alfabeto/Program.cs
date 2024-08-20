ae32902192b849ce2246a228dcc3078283d8b93a
Belscki <136617733+Belscki@users.noreply.github.com>
2024-08-20 10:22:56 -0300

    Seletor da letra do Alfabeto
    Achei mais fácil de fazer com C# do que com Python

---
+++ Alfabeto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite um número entre 1 e 26:");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int numero) && numero >= 1 && numero <= 26)
+        {
+            char letra = (char)('A' + numero - 1);
+            Console.WriteLine($"A letra correspondente ao número {numero} é: {letra}");
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida. Digite um número entre 1 e 26.");
+        }
+
+    }
+}
